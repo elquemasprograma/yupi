1fc8c6eebb3d9f245cded9133df73c936e5eb7ef
elquemasprograma <pucklover78@gmail.com>
2025-05-23 13:27:01 -0400

    Agregar conexion.php

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+	$host = "localhost";
+	$user = "root";
+	$password = "";
+	$db = "facturacion";
+
+	$connection = @mysqli_connect($host,$user,$password,$db);
+
+	if(!$connection){
+		echo "Error en la conexión";
+	}
+?>
