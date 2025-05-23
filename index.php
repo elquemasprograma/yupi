681d33eebc20b3e0673f8a179645d77c87948b7b
elquemasprograma <pucklover78@gmail.com>
2025-05-23 12:25:45 -0400

    Actualización en index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-	print "Hello World";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP & HTML</title>
+</head>
+<body>
+	<h1>Curso de PHP</h1>
+	<h2>Nivel Basico</h2>
+</body>
+</html>
+
+	<?php
+		//mensaje con print
+		print "<h1>Curso de PHP</h1>";
+		//mensaje con echo
+		echo "<h2>Nivel <br> Basico</h2>";		
+	?>
